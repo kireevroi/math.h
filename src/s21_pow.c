8f5b13461c8053c597d9e8173325eebaa3496266
Cormac Silver <cormacsi@at-r4.msk.21-school.ru>
2023-01-05 21:34:29 +0300

    pow

--- src/s21_pow.c
+++ src/s21_pow.c
@@ -7,7 +7,9 @@ long double s21_pow(double base, double exp) {
     result = S21_NAN;
   else if (base == S21_N_INF)
     result = S21_INF;
+  else if (base == 0. && exp == 0.)
+    result = 1;
   else
-    result = s21_exp((long double)exp * s21_log(base));
+    result = s21_exp((long double)exp * s21_log(fabs(base))); // FABS?
   return result;
 }
--- src/tests/test_pow.c
+++ src/tests/test_pow.c
@@ -1,12 +1,11 @@
 #include "test_me.h"
 
-// START_TEST(s21_pow_0) {
-// double num = 21.287287;
-// double exp = 7.453;
-// printf("%Lf -- %Lf\n", s21_pow(num, exp), powl(num, exp));
-// ck_assert_ldouble_eq_tol(s21_pow(num, exp), powl(num, exp), S21_EPS);
-// }
-// END_TEST
+START_TEST(s21_pow_0) {
+double a = 21.287287;
+double b = 7.453;
+ck_assert_ldouble_eq_tol(s21_pow(a, b), powl(a, b), S21_EPS);
+}
+END_TEST
 
 START_TEST(s21_pow_1) {
   double a = 0.;
@@ -17,23 +16,22 @@ END_TEST
 
 START_TEST(s21_pow_2) {
   double a = 3.46;
-  double b = 4.5;
+  double b = -4.5;
   ck_assert_ldouble_eq_tol(s21_pow(a, b), pow(a, b), S21_EPS);
 }
 END_TEST
 
-// START_TEST(s21_pow_3) {
-//   double a = -1.;
-//   double b = 2.;
-//   ck_assert_ldouble_eq_tol(s21_pow(a, b), pow(a, b), S21_EPS);
-// }
-// END_TEST
+START_TEST(s21_pow_3) {
+  double a = -6.;
+  double b = 2.;
+  ck_assert_ldouble_eq_tol(s21_pow(a, b), pow(a, b), S21_EPS);
+}
+END_TEST
 
 START_TEST(s21_pow_4) {
-  double a = -5.74;
-  double b = -7.2;
-  ck_assert_ldouble_nan(s21_pow(a, b));
-  ck_assert_ldouble_nan(pow(a, b));
+  double a = -5.;
+  double b = -3.5;
+  ck_assert_ldouble_eq_tol(s21_pow(a, b), pow(a, b), S21_EPS);
 }
 END_TEST
 
@@ -51,12 +49,12 @@ START_TEST(s21_pow_6) {
 }
 END_TEST
 
-// START_TEST(s21_pow_7) {
-//   double a = 0.;
-//   double b = 0.;
-//   ck_assert_ldouble_eq_tol(s21_pow(a, b), pow(a, b), S21_EPS);
-// }
-// END_TEST
+START_TEST(s21_pow_7) {
+  double a = 0.;
+  double b = 0.;
+  ck_assert_ldouble_eq_tol(s21_pow(a, b), pow(a, b), S21_EPS);
+}
+END_TEST
 
 START_TEST(s21_pow_8) {
   double a = INFINITY;
@@ -104,7 +102,14 @@ START_TEST(s21_pow_13) {
 }
 END_TEST
 
-// START_TEST(s21_pow_13) {
+START_TEST(s21_pow_14) {
+  double a = 3971328.213;
+  double b = 13.;
+  ck_assert_ldouble_eq_tol(s21_pow(a, b), pow(a, b), S21_EPS);
+}
+END_TEST
+
+// START_TEST(s21_pow_15) {
 //   srand(time(NULL));
 //   for (double num = -logl(DBL_MAX); num <= logl(DBL_MAX);
 //        num += (rand() % 10000) / 1000000. + 0.06) {
@@ -118,32 +123,25 @@ END_TEST
 // }
 // END_TEST
 
-// START_TEST(s21_pow_14) {
-//   double num = 3971328.213;
-//   ck_assert_ldouble_infinite(s21_pow(num));
-//   ck_assert_ldouble_infinite(pow(num));
-// }
-// END_TEST
-
 Suite *test_s21_pow(void) {
   Suite *s = suite_create("\033[45m-=S21_POW=-\033[0m");
   TCase *tc = tcase_create("s21_pow_tc");
 
-  // tcase_add_test(tc, s21_pow_0);
+  tcase_add_test(tc, s21_pow_0);
   tcase_add_test(tc, s21_pow_1);
   tcase_add_test(tc, s21_pow_2);
-  // tcase_add_test(tc, s21_pow_3);
+  tcase_add_test(tc, s21_pow_3);
   tcase_add_test(tc, s21_pow_4);
   tcase_add_test(tc, s21_pow_5);
   tcase_add_test(tc, s21_pow_6);
-  // tcase_add_test(tc, s21_pow_7);
+  tcase_add_test(tc, s21_pow_7);
   tcase_add_test(tc, s21_pow_8);
   tcase_add_test(tc, s21_pow_9);
   tcase_add_test(tc, s21_pow_10);
   tcase_add_test(tc, s21_pow_11);
   tcase_add_test(tc, s21_pow_12);
   tcase_add_test(tc, s21_pow_13);
-  // tcase_add_test(tc, s21_pow_14);
+  tcase_add_test(tc, s21_pow_14);
 
   suite_add_tcase(s, tc);
   return s;
