b629294c1ee1484e4bb3f1725e9b7f8a16331fd0
cormacsi <cormacsi@student.21-school.ru>
2023-01-13 20:26:52 +0300

    ceil

--- src/s21_ceil.c
+++ src/s21_ceil.c
@@ -7,14 +7,18 @@ long double s21_ceil(double a) {
   else if (a == S21_INF || a == S21_N_INF)
     res = a;
   else {
-    double b = (int)a;
+    double b = (long int)a;
+    // printf("%lf\n", b);
     long double comp_a = a * 10e18;
     long double comp_b = b * 10e18;
+    // printf("%Lf\n", comp_b);
     if (comp_a - comp_b > 0) {
-      res = (int)(comp_b / 10e18);
+      // res = (long int)(comp_b / 10e18);
+      res = b;
       res += 1.;
     } else {
-      res = (int)(comp_a / 10e18);
+      // res = (long int)(comp_a / 10e18);
+      res = b;
     }
   }
   return res;
