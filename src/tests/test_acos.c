58342861f6806dc7f89873fc148d9aa295548dc5
coriemis <coriemis@student.21-school.ru>
2023-01-13 17:21:59 +0400

    acos tests

--- src/tests/test_acos.c
+++ src/tests/test_acos.c
@@ -64,6 +64,22 @@ START_TEST(s21_acos_10) {
 }
 END_TEST
 
+START_TEST(s21_acos_11) {
+  srand(time(NULL));
+  for (double num = S21_EPS; num < 1;
+       num *= (rand() % 10000) / 1000000. + 1.01) {
+    ck_assert_ldouble_eq_tol(s21_acos(num), acos(num), S21_EPS);
+  }
+}
+
+START_TEST(s21_acos_12) {
+  srand(time(NULL));
+  for (double num = -S21_EPS; num > -1;
+       num *= (rand() % 10000) / 1000000. + 1.01) {
+    ck_assert_ldouble_eq_tol(s21_acos(num), acos(num), S21_EPS);
+  }
+}
+
 Suite *test_s21_acos(void) {
   Suite *s = suite_create("\033[45m-=S21_ACOS=-\033[0m");
   TCase *tc = tcase_create("s21_acos_tc");
@@ -78,6 +94,8 @@ Suite *test_s21_acos(void) {
   tcase_add_test(tc, s21_acos_8);
   tcase_add_test(tc, s21_acos_9);
   tcase_add_test(tc, s21_acos_10);
+  tcase_add_test(tc, s21_acos_11);
+  tcase_add_test(tc, s21_acos_12);
 
   suite_add_tcase(s, tc);
   return s;
